a440db3c86adc22cc7d130f424bd87d9fb753614
Ole Eskild Steensen <oleeskilds@gmail.com>
2021-02-13 22:08:16 +0100

    Correct fitbit url

--- src/components/SleepBlock.vue
+++ src/components/SleepBlock.vue
@@ -56,7 +56,7 @@ export default {
 
       },
       url:
-        "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22BB9N&redirect_uri=https%3A%2F%2Fole.dev%2Fdash%3A8080&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800"
+        "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22BB9N&redirect_uri=https%3A%2F%2Fole.dev%2Fdash&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800"
     };
   },
   mounted: function(){
